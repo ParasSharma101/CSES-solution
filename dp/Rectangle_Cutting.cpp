0062d647de1d6c61b412fab27287134822708374
PARAS SHARMA <145317890+ParasSharma101@users.noreply.github.com>
2025-04-25 20:34:50 +0530

    Implement code changes to enhance functionality and improve performance

--- dp/Rectangle_Cutting.cpp
+++ dp/Rectangle_Cutting.cpp
@@ -10,6 +10,35 @@ signed main() {
     ios::sync_with_stdio(false); 
     cin.tie(NULL);
     cout.tie(NULL);
+    int a, b;
+    cin >> a >> b;
 
+    vector<vector<int>> dp(a+1, vector<int>(b+1, 1e9));
+
+    for(int i = 1; i<= a; i++)
+    {
+        for(int j = 1; j <= b; j++)
+        {
+            if(i == j)
+            {
+                dp[i][j] = 0;
+                continue;
+            }
+
+            // horizontal cut
+            for(int h = 1; h <= i-1; h++)
+            {
+                dp[i][j] = min(dp[i][j], dp[h][j] + dp[i-h][j] + 1);
+            }
+            // vertical cut
+
+            for(int v = 1; v <= j-1;v++)
+            {
+                dp[i][j] = min(dp[i][j], dp[i][v] + dp[i][j-v]+1);
+            }
+        }
+    }
+    
+    cout << dp[a][b] ;
 }
 
